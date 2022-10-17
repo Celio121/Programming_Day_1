b90a4286cd356e12c083b0be227c76f764898f29
Celio121 <celiopereira751@gmail.com>
2022-10-17 16:25:14 +0100

    inter

--- Squares.py
+++ Squares.py
@@ -1,4 +1,7 @@
-num_loop = 0
-while num_loop > 100:
-    print(num_loop)
-    num_loop += 1**2
+number = 1
+while number < 100:
+    square = number ** 2
+    number += 1
+    if square >= 2000:
+        break
+    print(f"{number} ^2 = {square}")
